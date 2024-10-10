320503d9e14c23ffea29d38e2602aa14b1f6ac87
yokxim <yooksi53@gmail.com>
2024-10-10 11:55:36 +0900

    when same type bean exists

---
+++ src/test/java/com/sparta/springauth/BeanTest.java
@@ -0,0 +1,23 @@
+package com.sparta.springauth;
+
+import com.sparta.springauth.food.Food;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BeanTest {
+
+    @Autowired
+    @Qualifier("pizza")
+    Food food;
+
+    @Test
+    @DisplayName("테스트")
+    void 테스트1() {
+        food.eat();
+    }
+}
+
